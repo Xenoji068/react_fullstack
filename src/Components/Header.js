e33b996dbf94a6cfecc197c63650a27092b8ca35
Joshua-Owen <xenoji__@outlook.com>
2023-04-16 20:41:53 +0100

    how to add components and css

---
+++ src/Components/Header.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from "react";
+
+
+//let styles = {
+//    header: {
+//        background: '#03a9fa'
+//    },
+//    logo: {
+//        color: '#fff',
+//        fontFamily: 'Anton',
+//        textAlign: 'center'
+//    }
+//}
+
+
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <>
+                <header>
+                    <div className = "logo">Logo</div>
+                    <input/>
+                </header>
+
+            </>
+        )
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-
-
+import Header from './Components/Header';
+import './Styles/Styles.css'
 const App = () => (
-    <div>
-        <h1>Hello</h1>
+    <div className="hey">
+        <Header/>
 
     </div>
 
