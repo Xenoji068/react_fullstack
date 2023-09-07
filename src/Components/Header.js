c20fe51092182ddf66b18e00aea9f3c4d8492fcb
Xenoji <xenoji__@outlook.com>
2023-09-07 15:14:03 +0100

    back?

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,13 +3,25 @@ import React, { Component } from "react";
 class Header extends Component {
 
     state = {
+        active: 'active',
+        /*active: false*/ 
         keywords: ''
     }
 
 
     inputChange = (event) => {
+        {/*let value = event.target.value === '' ? false : true;*/}
         this.setState({
+            /*active: true*/
+            active: 'active',
             keywords: event.target.value
+        })
+    }
+
+    changeColor = () => {
+        this.setState({
+            active: this.state.active === 'active' ? 'not-active' : 'active'
+            /*            active: this.state.active ? 'false' : 'true'*/
         })
     }
 
@@ -17,9 +29,12 @@ class Header extends Component {
     render() {
         return (
             <>
-                <header>
+                {/*<header style ={{background: `${this.state.active ? 'red' : 'blue'}`}}>*/}
+                    <header className={this.state.active} style ={{background: `${this.state.active === 'active' ? 'red' : 'blue'}`}}>
+                    {/*<header className={this.state.active ? 'active' : 'non-active'}>*/}
                     <div className="logo">Code news</div>
                     <input onChange={(e) => this.inputChange(e)} />
+                    <button onClick={()=> this.changeColor()}>Change it</button>
                 </header>
             </>
         )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css'
 
-import Header from './components/header';
+import Header from './components/header.js';
 import NewsList from './components/news_list';
 import Footer from './components/footer';
 import JSON from './db.json';
@@ -11,15 +11,24 @@ import JSON from './db.json';
 class App extends Component {
 
     state = {
+        active: false,
         news:JSON,
         footerText:'I am a happy footer'
+    }
+
+    getKeywords =(event) =>{
+        /*console.log('Hey !!')*/
+        console.log(event.target.value)
     }
 
 
     render(){
         return(
             <div className="hey">
-                <Header/>
+
+                <Header
+                active ={this.active} 
+                keyword= {this.getKeywords}/>
                 <NewsList
                     news={this.state.news}
                 >
